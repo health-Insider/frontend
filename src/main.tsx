ccf5ddffd4441f9d71f94f461a4c1c379b6f418a
Prateek Kesavarapu <kesavarapu.prateek@gmail.com>
2023-10-27 16:39:19 +0530

    Finished hero page

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { NextUIProvider } from '@nextui-org/react';
 
 import App from './App.tsx';
-import './index.css';
+import './tailwind.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <NextUIProvider>
-            <App />
+            <main className='dark text-foreground bg-background height-max'>
+            <App/>
+            </main>
         </NextUIProvider>
     </React.StrictMode>
 );
--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,7 +1,27 @@
+import Nav from '../../components/navbar.jsx'
+import './index.css'
+import { Divider } from '@nextui-org/react'
+
 export default function Home() {
     return (
         <>
-        EA Sports, its imn the game!
+            <Nav/>
+            <div className="flex min-h-screen max-h-screen -mt-12 rounded-lg 
+                            bg-gradient-to-r from-kelp-green to-black" 
+                 id="hero-image">
+                <div className='flex self-center min-h-[40vh] max-w-[80vh] mx-[12vh] items-center p-10 justify-center' id = "box">
+                    <h1 className='text-left font-semibold'>Healthcare that adjusts to you</h1>
+                </div>
+                <div className='flex self-center min-h-[40vh] max-w-[80vh] mx-[12vh]'>
+                </div>
+            </div>
+            <div className='flex-1 p-5 text-white' id='body'>
+                <h3 className='font-bold text-[35px] my-2'>What we do</h3>
+                <Divider className="my-6"/>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at orci in nisi interdum sollicitudin. Etiam lobortis luctus nisl fringilla faucibus. Pellentesque fringilla, lacus ut tempus fringilla, urna massa ullamcorper nulla, vitae finibus ligula eros in libero. Nullam id quam hendrerit enim pretium consectetur quis id lectus. Duis eu enim et ex laoreet placerat eu ut orci. Nam ex urna, porttitor sed orci nec, venenatis vehicula tellus. Mauris a neque egestas, viverra ex vel, mollis nisi. Donec sed aliquam lacus. Donec sed est sodales, elementum odio eget, vulputate odio. Fusce lacus lectus, sollicitudin quis dignissim ac, sollicitudin tempus orci. Phasellus viverra elementum ante ut pellentesque. Donec molestie diam at lacus hendrerit dignissim. Duis cursus dolor ac erat gravida, et ultrices ligula vehicula.
+                </p>
+            </div>
         </>
     )
 }
