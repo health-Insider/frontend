551c276ac75fa12d95fad0c26aa8876a45fa2c17
YajTPG <yajtpg@gmail.com>
2023-10-27 11:09:27 +0530

    Fix

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+import { nextui } from '@nextui-org/react';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,tsx,jsx,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [nextui()],
+}
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {nextui} from '@nextui-org/react';
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nextui()],
+  plugins: [react()],
   content: [
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,tsx,jsx}'
   ],
