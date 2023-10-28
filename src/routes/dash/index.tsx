b9264c9dcf93c078791c5940455ba4f417189f24
greatesh <70007115+greatesh@users.noreply.github.com>
2023-10-28 11:37:45 +0530

    ggwp

--- src/routes/dash/index.tsx
+++ src/routes/dash/index.tsx
@@ -10,7 +10,84 @@ import CalorieConsumption from '../../components/calorieConsumption.jsx'
 //@ts-ignore
 import Sleep from '../../components/sleepTime.jsx'
 
+import Chart from 'chart.js/auto';
+
 export default function Dash() {
+    const data = [
+        { year: 2010, count: 10 },
+        { year: 2011, count: 20 },
+        { year: 2012, count: 15 },
+        { year: 2013, count: 25 },
+        { year: 2014, count: 22 },
+        { year: 2015, count: 30 },
+        { year: 2016, count: 28 },
+      ];
+
+      const data2 = [
+        { year: 2010, count: 20 },
+        { year: 2011, count: 10 },
+        { year: 2012, count: 35 },
+        { year: 2013, count: 15 },
+        { year: 2014, count: 42 },
+        { year: 2015, count: 10 },
+        { year: 2016, count: 38 },
+      ];
+        
+        window.onload = () => {
+        try{
+            new Chart(
+                document.getElementById('steps'),
+                {
+                  type: 'line',
+                  data: {
+                    labels: data.map(row => row.year),
+                    datasets: [
+                      {
+                        label: 'Steps',
+                        axisY:{
+                            labelFontSize: 10,
+                          },
+                        data: data.map(row => row.count),
+                        borderColor:"#ffffff",
+                        fontSize: 70,
+                      }
+                    ]
+                  },
+                  options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                fontSize: 100
+                            }
+                        }]
+                    }
+                }
+                }
+              );
+              new Chart(
+                document.getElementById('weight'),
+                {
+                  type: 'line',
+                  data: {
+                    labels: data2.map(row => row.year),
+                    datasets: [
+                      {
+                        label: 'Steps',
+                        data: data2.map(row => row.count),
+                        borderColor:"#ffffff"
+                      }
+                    ]
+                  },
+                  options:{
+                    responsive:true
+                  }
+                }
+              );
+        }
+        catch{
+            //give up
+        }
+    }
     return (
         <>
             <div className="grid grid-rows-2 grid-cols-3 min-h-screen max-h-screen rounded-lg bg-gradient-to-r from-royal-blue to-black items-center" id="hero-image">
@@ -30,6 +107,21 @@ export default function Dash() {
                     <Sleep/>
                 </div>
             </div>
+            
+            <div className='' >
+            <br /><br />
+            <h1 className='text-center text-3xl'>Weight</h1><br />
+            <canvas id="weight" className='!w-1/2 !h-1/2 m-auto '></canvas>
+            
+            <br /><br />
+            <h1 className='text-center text-3xl'>Steps</h1><br />
+            <canvas id="steps" className='!w-1/2 !h-1/2 m-auto '></canvas>
+
+            </div>
+
+
+
+
         </>
     );
 }
